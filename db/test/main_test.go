40875441873a25dc4b4c39515f855a6ba06a0273
Jan Nouza <nouzaj23@gmail.com>
2024-02-21 16:55:58 +0100

    Add viper to load config

--- db/test/main_test.go
+++ db/test/main_test.go
@@ -5,21 +5,20 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"study_lab/config"
 	db "study_lab/db/sqlc"
 	"testing"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:root@localhost:5432/study_lab_db?sslmode=disable"
 )
 
 var testQueries *db.Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	conf, err := config.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
+	testDB, err = sql.Open(conf.DBDriver, conf.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
 	}
--- main.go
+++ main.go
@@ -3,20 +3,19 @@ package main
 import (
 	"database/sql"
 	"log"
+	"study_lab/config"
 
 	_ "github.com/lib/pq"
 	"study_lab/api"
 	db "study_lab/db/sqlc"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5432/study_lab_db?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	conf, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
+	conn, err := sql.Open(conf.DBDriver, conf.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db: ", err)
 	}
@@ -24,7 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(conf.ServerAddress)
 
 	if err != nil {
 		log.Fatal("Cannot start server" + err.Error())
